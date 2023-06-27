d27f279e4d7781fdadc4fd3621adbba7650f3e88
Youmppy <headforesch00l@gmail.com>
2023-06-27 11:18:01 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - check the code
+ * @s:string
+ * Return: Always 0.
+ */
+int _strlen(char *s);
+{
+size_t strlen(const char *s);
+}
