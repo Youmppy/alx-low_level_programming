1e89587e6c06050130d67dd41325d6a34e32c061
Youmppy <headforesch00l@gmail.com>
2023-06-27 11:19:53 -0700

    ss

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strlen - check the code
+ * _strlen - length of code
  * @s:string
- * Return: Always 0.
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 size_t strlen(const char *s);
 }
