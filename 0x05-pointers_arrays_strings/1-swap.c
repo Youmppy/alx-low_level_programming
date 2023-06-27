9fb7795da0eba12d573a2d279e08090e0b5f8663
Youmppy <headforesch00l@gmail.com>
2023-06-27 10:38:22 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap 2 integers
+ * @a: int 1
+ * @b: int 2
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int m;
+int n;
+
+m = *a;
+n = *b;
+
+m = *b;
+n = *a;
+}
