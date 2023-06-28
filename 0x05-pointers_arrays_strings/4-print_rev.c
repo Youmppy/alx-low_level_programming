1ba8c3b1655dc6fe4ea76631688cc02b27c74f9f
Youmppy <headforesch00l@gmail.com>
2023-06-27 19:05:43 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints the  str
+ * @s: string
+ * Return: str
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0 ; *(s + i) != '\0' ; i++)
+		;
+
+	i = i - 1;
+	while (i != 0)
+	{
+		putchar(*(s + i));
+		i = i - 1;
+	}
+}
