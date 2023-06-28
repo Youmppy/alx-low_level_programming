c4b4301625e1e221b5d2171d11078fd5852e7450
Youmppy <headforesch00l@gmail.com>
2023-06-27 18:21:44 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints the  str
+ * @str: string
+ * Return: str
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+	putchar(*str);
+	str++;
+}
