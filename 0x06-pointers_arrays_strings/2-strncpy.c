ad40e99728735b509183b12290c0cc1887e86b7d
Youmppy <headforesch00l@gmail.com>
2023-06-29 20:02:20 -0700

    ss

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
+	int i = 0;
 
-while (*(dest + i) != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-return (src);
+	while (*(dest + i) != '\0')
+	{
+		dest[i] = src[i];
+	i++;
+	}
+	return (src);
 }
