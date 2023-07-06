4acbbb0728760cc28903dde20bfd8f9bdfcd9627
Youmppy <headforesch00l@gmail.com>
2023-07-06 12:55:16 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that checks for 
+ *
+ * Return: Always 0.
+ */
+
+void
+_puts_recursion (char *s)
+{
+  int i = 1;
+  if (*s != '\0')
+    {
+      _putchar (*s);
+      _puts_recursion (s + i);
+    }
+  else
+    {
+      _putchar ('\n');
+    }
+  return;
+}
