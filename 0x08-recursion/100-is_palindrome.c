2f52f288bba63a68772afb81b2cb1a5be65fd28c
Youmppy <headforesch00l@gmail.com>
2023-07-06 19:22:33 -0700

    101

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,39 +2,42 @@
 
 /**
  * _strlen_recursion - function that returns the length of a string
- *
+ * @s: int
  * Return: Length of the string.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
  * checker - Recursive helper function to check if a string is a palindrome
- *
+ * @s: int
+ * @d: int
+ * @f: int
  * Return: 1 if palindrome, 0 if not.
  */
 int checker(char *s, int d, int f)
 {
 	if (d >= f)
-		return 1;
+		return (1);
 	if (s[d] == s[f])
-		return checker(s, d + 1, f - 1);
+		return (checker(s, d + 1, f - 1));
 	else
-		return 0;
+		return (0);
 }
 
 /**
  * is_palindrome - function that checks if a string is a palindrome
- *
+ * @s: int
  * Return: 1 if palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
 	int n = _strlen_recursion(s);
-	return checker(s, 0, n - 1);
+
+	return (checker(s, 0, n - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,25 +3,26 @@
 /**
  * helpc - function that helps with the procedure
  * @n: int
+ * @i: int
  * Return: Always 0.
  */
 int helpc(int n, int i)
 {
-	if (n % i ==0 && i == n)
+	if (n % i == 0 && i == n)
 	{
 		return (1);
 	}
-	if (n % i ==0 && i != n)
+	if (n % i == 0 && i != n)
 	{
-		return(0);
+		return (0);
 	}
-	if(i>n)
+	if (i > n)
 	{
-		return(0);
+		return (0);
 	}
-	if(n % i !=0) 
+	if (n % i != 0)
 	{
-		return(helpc(n,i+1));
+		return (helpc(n, i + 1));
 	}
 }
 
@@ -33,5 +34,5 @@ int helpc(int n, int i)
 
 int is_prime_number(int n)
 {
-	return (helpc(n,2));
+	return (helpc(n, 2));
 }
