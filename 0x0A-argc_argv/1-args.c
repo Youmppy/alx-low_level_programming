b37e4592e1919d89b5c2a340b4ec196e258e2c8e
Youmppy <headforesch00l@gmail.com>
2023-07-07 19:19:23 -0700

    ss

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argv: arg value
+ * @argc: arg count
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argv: arg value
+ * @argc: arg count
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = argc;
+
+	while(i > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	
+	return (0);
+}
+
