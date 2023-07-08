189e2bec0cd6f996ec073c4fe06712125bd90e6e
Youmppy <headforesch00l@gmail.com>
2023-07-07 19:51:17 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,9 +10,12 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
 		return (0);
 	}
 	else
