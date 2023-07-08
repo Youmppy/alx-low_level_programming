07327a5fe49cddeb24a66a6de1cb868777567262
Youmppy <headforesch00l@gmail.com>
2023-07-07 19:01:13 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * mynameis - prints name
  * @argv: arg value
+ * @argc: arg count
  * Return: nothing
  */
 
-void mynameis(char **argv)
+int main (int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf ("%s",argv[0]);
+	return 0;
 }
