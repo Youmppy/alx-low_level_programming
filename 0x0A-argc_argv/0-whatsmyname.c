d4c34deeabfe600cbff24ef4bc6cee5d88b108c1
Youmppy <headforesch00l@gmail.com>
2023-07-07 19:12:13 -0700

    ss

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * main - prints name
  * @argv: arg value
  * @argc: arg count
- * Return: nothing
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
