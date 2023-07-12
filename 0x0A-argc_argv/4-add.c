d1efe5e6de5790d2556de0cc491219ae6742a8bf
Youmppy <headforesch00l@gmail.com>
2023-07-12 15:55:25 -0700

    ss

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,52 +1,49 @@
 #include "main.h"
 #include <stdio.h>
-#include  <stdlib.h>
+#include <stdlib.h>
 
 /**
- * main - prints int 1 + int 2
- * @argv: arg value
- * @argc: arg count
- * Return: 0 true 1 faulse
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
 {
-int i = 1;
-int n = 0;
-int error = 0;
-if (argc == 1)
-{
-printf("Error\n");
-return (1);
-}
+	int i = 1;
+	int n = 0;
+	int error = 0;
 
-while (i < argc)
-{
-	int j = 0;
-
-	while (argv[i][j] != '\0')
+	if (argc == 1)
 	{
-		if (argv[i][j] < '0' || argv[i][j] > '9')
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		int j = 0;
+
+		while (argv[i][j] != '\0')
 		{
-			error = 1;
-			break;
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				error = 1;
+				break;
+			}
+			j++;
 		}
-		j++;
+
+		if (error)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		n = n + atoi(argv[i]);
+		i++;
 	}
-	if (error != 1)
-	{
-	n = n + atoi(argv[i]);
-	i++;
-	}
-}
-if (error == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-else
-{
+
 	printf("%d\n", n);
 	return (0);
-}
 }
