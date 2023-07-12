9e3780d47b1d618b85b5b58631c11798955a1b5a
Youmppy <headforesch00l@gmail.com>
2023-07-12 16:05:03 -0700

    ss

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,46 @@
 #include "main.h"
 #include <stdio.h>
-#include  <stdlib.h>
+#include <stdlib.h>
 
 /**
- * main - prints int 1 + int 2
- * @argv: arg value
- * @argc: arg count
- * Return: 0 true 1 false
+ * main - prints how many coins are needed for change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
 {
-int i = argc;
-int n =  atoi(argv[1]);
-int c = 0;
+	int n, c = 0;
 
-while(n != 0)
-{
-	else if( n >= 25)
+	if (argc != 2)
 	{
-		n= n - 25;
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (n > 0)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else if (n >= 1)
+			n -= 1;
+
 		c++;
 	}
-	else if( n >= 2)
-	{
-		n= n - 25;
-		c++;
-	}
-	else if (n >= 5)
-	{
-		n= n - 5;
-		c++;
-	}
-	else if (n >= 10)
-	{
-		n= n - 10;
-		c++;
-	}
-	else if (n >= 1)
-	{
-		n= n - 1;
-		c++
-	}
-}
-printf("%d\n",c);
-return (0);
+
+	printf("%d\n", c);
+	return (0);
 }
