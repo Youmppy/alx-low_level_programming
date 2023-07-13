e590c96726cd4cd2f9955263d6b875c4b2dc4dde
Youmppy <headforesch00l@gmail.com>
2023-07-12 19:08:14 -0700

    ss

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,47 +7,35 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: Nothing.
+ * Return: Pointer to the concatenated string, or NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int n = 0;
-	int k = 0;
-	int l = 0;
-	char *ptr;
+	int i = 0, j = 0, k = 0;
+	char *concatenated;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
-	while (s2[n] != '\0')
-	{
-		n++;
-	}
-	if (s1 == 0 && s2 == 0)
-	{
+	while (s2[j] != '\0')
+		j++;
+
+	concatenated = malloc((i + j + 1) * sizeof(char));
+	if (concatenated == NULL)
 		return (NULL);
-	}
-	else
-	{
-		ptr = malloc(i + n);
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-		while (k != i)
-		{
-			ptr[k] = s1[k];
-			k++;
-		}
-		while (l != n)
-		{
-			ptr[k] = s1[l];
-			l++;
-			k++;
-		}
-		return (ptr);
-	}
+
+	while (*s1 != '\0')
+		concatenated[k++] = *s1++;
+
+	while (*s2 != '\0')
+		concatenated[k++] = *s2++;
+
+	concatenated[k] = '\0';
+
+	return (concatenated);
 }
