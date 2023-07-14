abc709b77be50d88eec2541fc5dc14978040d689
Youmppy <headforesch00l@gmail.com>
2023-07-14 16:43:32 -0700

    ss

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - creates an array
+ * @b: int
+ *
+ * Return: Nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		return (98);
+	}
+	return (ptr);
+}
