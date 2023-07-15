c96769543acd40dfc3326ab92997af1766058959
Youmppy <headforesch00l@gmail.com>
2023-07-14 17:12:33 -0700

    ss

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_nconcat - add 2 str
+ * @s1: str 1
+ * @s2: str 2
+ * @n: length
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	int i = 0;
+	char *ptr;
+
+	ptr = malloc(n * 2);
+
+	if (ptr == NULL)
+	{
+		exit(NULL);
+	}
+
+	while (i != n)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i != n)
+	{
+		ptr[i] = s2[i];
+		i++;
+	}
+
+	return (ptr);
+}
