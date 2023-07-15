d58d0d0db679ae6a6e4e2c904a55dfc0b7c1b411
Youmppy <headforesch00l@gmail.com>
2023-07-14 17:25:25 -0700

    ss

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,37 +1,44 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * string_nconcat - add 2 str
- * @s1: str 1
- * @s2: str 2
- * @n: length
- * Return: 0
+ * string_nconcat - concatenates two strings up to n bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: maximum number of bytes from s2 to concatenate
+ * Return: pointer to the concatenated string
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
-	char *ptr;
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat;
 
-	ptr = malloc(n * 2);
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
 
-	if (ptr == NULL)
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (concat == NULL)
 	{
-		exit(NULL);
+		printf("Memory allocation failed\n");
+		return (NULL);
 	}
 
-	while (i != n)
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (i != n)
-	{
-		ptr[i] = s2[i];
-		i++;
-	}
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-	exit(ptr);
+	for (j = 0; j < n; j++, i++)
+		concat[i] = s2[j];
+
+	concat[i] = '\0';
+
+	return (concat);
 }
+
