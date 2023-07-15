cac77dd7b743c683da974d33fb58526aec48f8b3
Youmppy <headforesch00l@gmail.com>
2023-07-14 17:34:53 -0700

    ss

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,19 +11,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int total_size;
+	void *ptr;
+	unsigned int total_size;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total_size = nmemb * size;
-    ptr = malloc(total_size);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
 
-    if (ptr == NULL)
-        return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-    memset(ptr, 0, total_size);
+	memset(ptr, 0, total_size);
 
-    return (ptr);
+	return (ptr);
 }
