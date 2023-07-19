b329db660368603be1a25653a6cd247ea68164fa
Youmppy <headforesch00l@gmail.com>
2023-07-18 19:09:38 -0700

    ss

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a fog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owne: dog's owner
+ *
+ */
 struct dog {
     char *name;
     float age;
