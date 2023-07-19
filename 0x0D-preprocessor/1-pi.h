8fb5cb9141df7863e4cd9889d16f42e33f42573f
Youmppy <headforesch00l@gmail.com>
2023-07-18 18:19:39 -0700

    ss

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef DPI_H
+#define DPI_H
+
+#define PI 3.14159265359
+
+#endif /* DPI_H */
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Nothing
+ */
+void  main(void)
+{
+	printf("%s\n",__FILE__);
+}
