d7e6dec7579f0713e03dfaa0b283138a3a15f417
Youmppy <headforesch00l@gmail.com>
2023-07-20 17:46:57 -0700

    ss

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list args;
+	va_list ptr;
 
-	va_start(args, n);
+	va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(ptr, int);
 	}
 
-	va_end(args);
+	va_end(ptr);
 	return (sum);
 }
