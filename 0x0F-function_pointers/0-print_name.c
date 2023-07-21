67ac5bdd8ceb9f23b15e8aa917c283f294aab66e
Youmppy <headforesch00l@gmail.com>
2023-07-21 16:57:23 -0700

    ss

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,16 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
+ * @f: function
  *
  * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
 	{
 		(*f)(name);
 	}
