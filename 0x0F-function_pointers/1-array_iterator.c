04aee40470900b680a69ea86eeeb57e4ea80e34f
Youmppy <headforesch00l@gmail.com>
2023-07-21 17:35:59 -0700

    ss

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,8 +16,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && size != NULL && action != NULL)
 	{
-	for(i = 0; i < size; i++)
-	{
-		action(array[i]);
+		for(i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
