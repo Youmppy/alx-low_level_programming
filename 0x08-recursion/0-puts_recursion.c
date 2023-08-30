ae0e7266fda78e7ab38ccc6a03fdc001ce401167
Youmppy <HeadforeSch00l@gmail.com>
2023-08-30 15:28:16 -0700

    zero

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts_recursion - Prints a string using recursion.
+ * @s: The input string to be printed.
+ *
+ * Description: This function recursively prints each character
+ * of the input string until the null terminator is reached.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
