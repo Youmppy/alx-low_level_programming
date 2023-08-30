11183a7e3e6170ada22f484d3c4b3b8c61eefb9d
Youmppy <HeadforeSch00l@gmail.com>
2023-08-30 15:59:47 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,18 +7,15 @@
  *
  * Return: The factorial of the number.
  *
- * Description: This function recursively calculates the factorial of the input number.
+ * Description:calculates the factorial of the input number.
  */
 int factorial(int n)
 {
-	int f = 0;
 
 	if (n == 0)
 		return (1);
 	if (n < 0)
 		return (-1);
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 }
+
