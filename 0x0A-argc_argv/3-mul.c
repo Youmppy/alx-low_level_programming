5808716fc5c1d67cefad00cc3a96e1bbb46e9da8
Youmppy <HeadforeSch00l@gmail.com>
2023-09-01 16:48:54 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,22 +6,23 @@
  * @argc: The number of command-line arguments.
  * @argv: An array containing the command-line arguments.
  *
- * Return: 0 on success.
+ * Return: 0 on success, 1 on error.
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-char i = 0;
-
-	if (argc == 3)
-	{
-		i = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", i);
-		return (0);
-	}
 	if (argc != 3)
 	{
-	printf("Error\n");
-	return (1);
+		/* Print an error message and return 1 if argc is not 3 */
+		printf("Error\n");
+		return (1);
 	}
-	return (1);
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	/* Print the result of the multiplication */
+	printf("%d\n", result);
+
+	return 0;
 }
