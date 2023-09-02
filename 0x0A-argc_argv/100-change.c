ceebd56d9c17b7885e3895f0deac28bc54ed6140
Youmppy <HeadforeSch00l@gmail.com>
2023-09-01 18:19:34 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		/* Print an error message and return 1 if argc is not 3 */
+		printf("Error\n");
+		return (1);
+	}
+
+	int n = atoi(argv[1]);
+	int i = 0;
+
+	if (n > 0)
+	{
+		printf("0\n");
+	}
+	for (i = 0; n != 0; i++)
+	{
+		if (n >= 25)
+			n = n - 25;
+		if (n >= 10)
+			n = n - 10;
+		if (n >= 5)
+			n = n - 5;
+		if (n >= 1)
+			n = n - 1;
+	}
+
+	/* Print the result of the multiplication */
+	printf("%d\n", n);
+
+	return (0);
+}
+
