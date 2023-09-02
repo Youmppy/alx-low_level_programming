e39fe6d0055b59cb73f6c78383920f8e402e1064
Youmppy <HeadforeSch00l@gmail.com>
2023-09-01 19:21:04 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
 int main(int argc, char *argv[])
 {
-    int amount, num_coins, count, i;
-    int coins[] = {25, 10, 5, 2, 1};
+	int amount;
+	int num_coins;
+	int count;
+	int i;
+	int coins[] = {25, 10, 5, 2, 1};
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    amount = atoi(argv[1]);
+	amount = atoi(argv[1]);
 
-    if (amount < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
+	if (amount < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    num_coins = sizeof(coins) / sizeof(coins[0]);
-    count = 0;
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
 
-    for (i = 0; i < num_coins; i++)
-    {
-        while (amount >= coins[i])
-        {
-            amount -= coins[i];
-            count++;
-        }
-    }
+	for (i = 0; i < num_coins; i++)
+	{
+		while (amount >= coins[i])
+		{
+			amount -= coins[i];
+			count++;
+		}
+	}
 
-    printf("%d\n", count);
+	printf("%d\n", count);
 
-    return 0;
+	return (0);
 }
