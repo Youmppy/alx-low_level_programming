6313a2f4d779e0fd1e264c6f411e2bb04b8405f0
Youmppy <HeadforeSch00l@gmail.com>
2023-09-01 19:05:38 -0700

    ss

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,35 +10,37 @@
  */
 int main(int argc, char *argv[])
 {
-	int n = 0;
-	int i = 0;
-
 	if (argc != 2)
 	{
-		/* Print an error message and return 1 if argc is not 3 */
+		/* Print an error message and return 1 if argc is not 2 */
 		printf("Error\n");
 		return (1);
 	}
-	n = atoi(argv[1]);
-	if (n < 0)
+
+	int amount = atoi(argv[1]);
+
+	if (amount < 0)
 	{
+		/* If the input amount is negative, print 0 and return 0 */
 		printf("0\n");
 		return (0);
 	}
-	while (n != 0)
+
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int count = 0;
+
+	for (int i = 0; i < num_coins; i++)
 	{
-		if (n >= 25)
-			n = n - 25;
-		if (n >= 10)
-			n = n - 10;
-		if (n >= 5)
-			n = n - 5;
-		if (n >= 2)
-			n = n - 2;
-		if (n >= 1)
-			n = n - 1;
-		i++;
+		while (amount >= coins[i])
+		{
+			amount -= coins[i];
+			count++;
+		}
 	}
-	printf("%d\n", i);
+
+	/* Print the minimum number of coins required */
+	printf("%d\n", count);
+
 	return (0);
 }
