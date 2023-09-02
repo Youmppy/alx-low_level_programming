482a4cfc18c7ca22d4da6e480f8c2b2b1e667e3a
Youmppy <HeadforeSch00l@gmail.com>
2023-09-01 18:42:29 -0700

    101

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,7 +19,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	n = atoi(argv[1]);
 	if (n < 0)
 	{
@@ -29,29 +28,16 @@ int main(int argc, char *argv[])
 	for (i = 0; n > 0; i++)
 	{
 		if (n >= 25)
-		{
 			n = n - 25;
-		}
 		if (n >= 10)
-		{
 			n = n - 10;
-		}
 		if (n >= 5)
-		{
 			n = n - 5;
-		}
 		if (n >= 2)
-		{
 			n = n - 2;
-		}
 		if (n >= 1)
-		{
 			n = n - 1;
-		}
 	}
-
-	/* Print the result of the multiplication */
 	printf("%d\n", i);
-
 	return (0);
 }
