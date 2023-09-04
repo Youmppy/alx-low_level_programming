49d4526a47b243a1b3670e0e836feebaec6edbbe
Youmppy <HeadforeSch00l@gmail.com>
2023-09-04 07:36:10 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	array = maloc(size);
-	if(size == 0||array == NULL)
-		return(NULL);
-	for(i = 0; i < size; i++)
+	if (size == 0 || array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
