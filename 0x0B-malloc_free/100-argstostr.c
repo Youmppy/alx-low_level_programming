241dc93324ef32f776120e3b16817e7195e5f994
Youmppy <HeadforeSch00l@gmail.com>
2023-09-05 17:07:37 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - Frees a 2D grid of integers.
+ * @grid: The pointer to the grid to be freed.
+ * @height: The height of the grid.
+ *
+ * This function frees the memory occupied by a 2D grid previously allocated
+ * using alloc_grid.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *array;
+	
+	if (ac ==0 || av == NULL)
+		return (NULL);
+	
+	array = malloc(ac);
+
+	if (array == NULL)
+		return (NULL);
+}
+
