c3fe53a7e6e46d240bbc6024fb531cdee2a20f00
Youmppy <HeadforeSch00l@gmail.com>
2023-09-05 17:15:01 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,24 +3,45 @@
 #include "main.h"
 
 /**
- * free_grid - Frees a 2D grid of integers.
- * @grid: The pointer to the grid to be freed.
- * @height: The height of the grid.
+ * argstostr - Concatenates all the arguments of your program.
+ * @ac: The number of arguments.
+ * @av: An array of pointers to the arguments.
  *
- * This function frees the memory occupied by a 2D grid previously allocated
- * using alloc_grid.
+ * Return: A pointer to the concatenated string, or NULL if memory allocation fails.
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
-	char *array;
-	
-	if (ac ==0 || av == NULL)
-		return (NULL);
-	
-	array = malloc(ac);
+	int i, j, len = 0, total_len = 0;
+	char *array, *concatenated;
 
-	if (array == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		total_len += len + 1; /* Add 1 for the newline character */
+		len = 0;
+	}
+
+	concatenated = malloc(total_len * sizeof(char));
+
+	if (concatenated == NULL)
+		return (NULL);
+
+	array = concatenated;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*array = av[i][j];
+			array++;
+		}
+		*array = '\n'; /* Add a newline character after each argument */
+		array++;
+	}
+
+	return (concatenated);
 }
-
