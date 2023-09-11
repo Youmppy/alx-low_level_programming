81bc5345a911ae2f61bcdf486523ea7a298ec749
Youmppy <HeadforeSch00l@gmail.com>
2023-09-11 15:07:45 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: bla
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
